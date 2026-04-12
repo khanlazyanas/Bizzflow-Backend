65bad9df0710ef0e3804c1742fe6ccabd46f1a3a
Anas Khan <anaskhan995620@gmail.com>
2026-04-13 00:37:48 +0530

    update util folder

--- utils/sendCookie.js
+++ utils/sendCookie.js
@@ -1,8 +1,7 @@
-import jwt from 'jsonwebtoken'; // Isko import karna zaroori hai
+import jwt from 'jsonwebtoken';
 
 export const sendCookie = (user, statusCode, res, message) => {
-  // 1. Token yahin generate karo (model se mangne ki zaroorat nahi)
-  // Make sure tumhari .env file mein JWT_SECRET likha ho
+  // 1. Token yahin generate karo
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: '15d', // Token 15 din tak chalega
   });
@@ -15,10 +14,13 @@ export const sendCookie = (user, statusCode, res, message) => {
     sameSite: "none",    // <-- ZAROORI HAI: Cross-origin allow karega
   };
 
-  // 3. Cookie set karo aur response bhejo
+  // 3. SECURITY FIX: Password hash ko frontend me bhejne se roko!
+  user.password = undefined;
+
+  // 4. Cookie set karo aur response bhejo
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
     message,
-    user,
+    user, // Ab user object ke andar 'fullName' aayega, bina password ke
   });
 };
