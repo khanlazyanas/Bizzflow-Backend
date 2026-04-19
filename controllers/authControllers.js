93642934ed67e0ac78080fc01c2967d47f2a99cf
Anas Khan <anaskhan995620@gmail.com>
2026-04-19 20:00:40 +0530

    fix cloudinary problem

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -8,9 +8,9 @@ import path from 'path';
 
 // --- CLOUDINARY CONFIGURATION ---
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'dfobd7fvw',
+  api_key: '664186533374346', 
+  api_secret: 'QtGeDxlazzgIJBubYQ18gUr2cO8'
 });
 
 // --- REGISTER USER ---
