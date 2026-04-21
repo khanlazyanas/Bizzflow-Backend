52afd88886e709cee3a54315ab77ddf89cd7c5aa
Anas Khan <anaskhan995620@gmail.com>
2026-04-21 15:58:16 +0530

    update in sendCookie.js

--- utils/sendCookie.js
+++ utils/sendCookie.js
@@ -1,23 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 export const sendCookie = (user, statusCode, res, message) => {
+
+  console.log("Mera JWT Secret hai: ", process.env.JWT_SECRET);
   // Token generate karo
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: '15d', 
   });
 
-  // 🔥 FIX: Environment ke hisaab se cookie ki security set karo
-  // Agar laptop par ho toh development, server par ho toh production
-  const isProduction = process.env.NODE_ENV === 'production';
-
+  // Cookie options
   const options = {
     expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000), 
     httpOnly: true,
-    secure: isProduction,                      // Localhost pe false, Server pe true
-    sameSite: isProduction ? "none" : "lax",   // Localhost pe "lax", Server pe "none"
+    secure: true,      
+    sameSite: "none",  
   };
 
-  // Password ko remove karke User data ko frontend me bhej do
+  // FIX: Password ko null karke User data ko frontend me bhej do
   user.password = undefined;
 
   res.status(statusCode).cookie("token", token, options).json({
